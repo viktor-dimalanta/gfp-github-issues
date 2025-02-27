97074819fddd02a59f5ec8b9eb7f012945698e90
Viktor Dimalanta <viktor.d@betterhealthcompany.com.au>
2025-02-27 15:15:17 +0800

    Fix view to table

--- resources/views/github/issues.blade.php
+++ resources/views/github/issues.blade.php
@@ -3,15 +3,55 @@
 @section('content')
 <div class="container">
     <h1>My Open GitHub Issues</h1>
-    <ul>
-        @forelse ($issues as $issue)
-            <li>
-                <strong>#{{ $issue['number'] }}</strong> - 
-                {{ $issue['title'] }} (Created: {{ \Carbon\Carbon::parse($issue['created_at'])->diffForHumans() }})
-            </li>
-        @empty
-            <p>No open issues assigned to you.</p>
-        @endforelse
-    </ul>
+    <br>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Issue #</th>
+                <th>Title</th>
+                <th>Created</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($issues as $issue)
+                <tr data-bs-toggle="modal" data-bs-target="#issueModal{{ $issue['number'] }}">
+                    <td><strong>#{{ $issue['number'] }}</strong></td>
+                    <td>{{ $issue['title'] }}</td>
+                    <td>{{ \Carbon\Carbon::parse($issue['created_at'])->diffForHumans() }}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3" class="text-center">No open issues assigned to you.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </div>
+
+<!-- Modal Template -->
+@foreach ($issues as $issue)
+    <div class="modal fade" id="issueModal{{ $issue['number'] }}" tabindex="-1" aria-labelledby="issueModalLabel{{ $issue['number'] }}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="issueModalLabel{{ $issue['number'] }}">Issue #{{ $issue['number'] }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p><strong>Title:</strong> {{ $issue['title'] }}</p>
+                    <p style="overflow: hidden;text-overflow: ellipsis;"><strong>Description:</strong> {{ $issue['body'] ?? 'No description available.' }}</p>
+                    <p><strong>Created:</strong> {{ \Carbon\Carbon::parse($issue['created_at'])->toFormattedDateString() }}</p>
+                    <p><strong>Updated:</strong> {{ \Carbon\Carbon::parse($issue['updated_at'])->toFormattedDateString() }}</p>
+                    <p><strong>Status:</strong> {{ $issue['state'] }}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <a href="{{ $issue['html_url'] }}" class="btn btn-primary" target="_blank">View on GitHub</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach
+
+
 @endsection
